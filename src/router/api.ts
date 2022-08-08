1b7103f46c004a2ec0fe81aaa94d36d6d94bfcdd
mys1024 <mys.1024@outlook.com>
2022-08-09 04:43:42 +0800

    feat: url validating

--- src/router/api.ts
+++ src/router/api.ts
@@ -4,6 +4,7 @@ import urlDao from "../dao/url.ts";
 const router = new oak.Router();
 
 router.post("/url", async (ctx) => {
+  // read request body
   const body = await ctx.request.body().value;
   if (!body) {
     ctx.response.status = 400;
@@ -14,7 +15,14 @@ router.post("/url", async (ctx) => {
     ctx.response.status = 400;
     return;
   }
+  // validate url
+  if (!/^(http|https):\/\/[^ \t]+$/.test(url)) {
+    ctx.response.status = 403;
+    return;
+  }
+  // store url
   const key = await urlDao.add(url);
+  // response
   ctx.response.body = {
     key,
     url,
