e5198a5ef2878e8639c9dd96829f2094b18b6303
mys1024 <mys.1024@outlook.com>
2022-08-10 22:45:53 +0800

    feat: use new env variable `DB_NAME`

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,12 +4,13 @@ import output from "../util/output.ts";
 const client = new MongoClient();
 
 const clusterUrl = Deno.env.get("DB_CLUSTER_URL") || config["DB_CLUSTER_URL"];
+const dbName = Deno.env.get("DB_NAME") || config["DB_NAME"];
 const username = Deno.env.get("DB_USERNAME") || config["DB_USERNAME"];
 const password = Deno.env.get("DB_PASSWORD") || config["DB_PASSWORD"];
 
 output.info(`Connecting to MongoDB...`);
 const db = await client.connect(
-  `mongodb+srv://${username}:${password}@${clusterUrl}/data?retryWrites=true&w=majority&authMechanism=SCRAM-SHA-1`,
+  `mongodb+srv://${username}:${password}@${clusterUrl}/${dbName}?retryWrites=true&w=majority&authMechanism=SCRAM-SHA-1`,
 );
 
 export default db;
